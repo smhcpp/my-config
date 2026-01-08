5e607ab4d7132156887f53440cb5e0be6c0c87f1
Morteza Hosseini <smhroot@gmail.com>
2026-01-08 10:40:03 +0100

    created backup config script

---
+++ backup-config.fish
@@ -0,0 +1,7 @@
+#!/usr/bin/env fish
+cp /etc/nixos/* ~/GitHub/my-config/nixos/
+cp ~/.config/niri/* ~/GitHub/my-config/niri/
+cp ~/.config/foot/* ~/GitHub/my-config/foot/
+cp ~/.config/fish/config.fish ~/GitHub/my-config/fish/
+cp ~/.config/nvim/* ~/GitHub/my-config/nvim/
+cp ~/.config/helix/* ~/GitHub/my-config/helix/
