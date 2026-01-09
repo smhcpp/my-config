863ba721afc3d272bb10481b30a56d9bd164a31c
Morteza Hosseini <smhroot@gmail.com>
2026-01-10 00:01:12 +0100

    update last config

--- nixos/hardware-configuration.nix
+++ nixos/hardware-configuration.nix
@@ -28,4 +28,8 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+  hardware.graphics = {
+    enable = true;
+    enable32Bit = true; # Often helps with driver path resolution
+  }; 
 }
